20f5a4d1d5615b0b75a4aa489441f0d580a02c06
Otavio Marques <marquesotavio251204@gmail.com>
2023-11-10 09:29:27 -0300

    init

---
+++ src/test/java/br/com/otavio/desafiobossabox/DesafioBossaboxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.otavio.desafiobossabox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBossaboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
